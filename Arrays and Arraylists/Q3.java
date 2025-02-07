8a964d5897f9cecdc3652f5263a7231e06c544d1
NkanyisoMz <sergeantmzobe@gmail.com>
2025-02-07 04:06:42 +0200

    Added implimantation of finding minimum value

---
+++ Arrays and Arraylists/Q3.java
@@ -0,0 +1,26 @@
+//Return a minimum from an array
+public class MinumunValue {
+
+    public static void printArray(int[] arr){
+        int n = arr.length;
+        for (int i = 0; i < n; i++){
+            System.out.print(arr[i] );
+        }
+        System.out.println();
+    }
+    public static int minimumValue(int[] arr){
+        int min = arr[0];
+        for (int i = 1; i < arr.length;i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+
+        return min;
+    }
+    public static void main(String[] args) {
+        int[] array = {7,3,5,1,1};
+        printArray(array);
+        System.out.println(minimumValue(array));
+    }
+}
